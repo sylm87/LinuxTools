8aec85b1e55e6dfbf8190ab8c543a0c131cf162f
sylm87 <christian472006@gmail.com>
2025-05-25 10:43:29 +0200

    code vs codium

--- provisioning/kali/generic/PostSeed.sh
+++ provisioning/kali/generic/PostSeed.sh
@@ -93,16 +93,6 @@ apt install python3 python3-venv -y
 echo -e "${Blue}[*] Installing git${ColorOff}"
 apt install git -y
 
-# Visual studio Code
-echo -e "${Blue}[*] Installing Visual Studio Code${ColorOff}"
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-apt install apt-transport-https -y
-apt update -y
-apt install code -y
-
 # Go
 echo -e "${Blue}[*] Installing go and dependencies (BETA)${ColorOff}"
 apt install golang-go -y
@@ -144,6 +134,18 @@ mkdir -p /tmp/Downloads
 wget --no-check-certificate -O /tmp/Downloads/dbeaber-ce.deb https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
 apt install /tmp/Downloads/dbeaber-ce.deb -y
 rm /tmp/Downloads/dbeaber-ce.deb
+
+# VSCodium
+echo -e "${Blue}[*] Installing VSCodium${ColorOff}"
+mkdir -p /tmp/Downloads
+curl -s -k https://api.github.com/repos/VSCodium/vscodium/releases/latest \
+| grep "browser_download_url.*amd64\.deb\"" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget --no-check-certificate -O /tmp/Downloads/vscodium-amd64.deb -qi -
+apt install /tmp/Downloads/vscodium-amd64.deb -y
+rm -r /tmp/Downloads
+ln -s $(which codium) /usr/bin/code
 
 # Web applications tools
 # Nuclei
